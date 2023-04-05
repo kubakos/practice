8835a3081fc8447de51d8dd75ae31ff80fba870f
Akos Kubak <kubakakos@gmail.com>
2023-04-05 13:28:08 +0200

    problem b test file done

--- test/test_problem_b.py
+++ test/test_problem_b.py
@@ -5,8 +5,18 @@ from problems.b.problem_b import *
 
 class TestB(unittest.TestCase):
 
-    def test_honeycomb_graph(self):
-        pass
+    def test_problem_b(self):
+        a = open('problems/b/sample-data/1.ans', 'r')
+        answer = a.read()
+        a.close()
+        hc = Honeycomb()
+        self.assertEqual(hc.search(input_data()), answer)
+
+        a = open('problems/b/sample-data/2.ans', 'r')
+        answer = a.read()
+        a.close()
+        hc = Honeycomb()
+        self.assertEqual(hc.search(input_data()), answer)
 
 
 if __name__ == '__main__':
