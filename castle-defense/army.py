90c884b273bf1a24025b2490b769de4b8a989df1
Akos Kubak <kubakakos@gmail.com>
2023-12-23 01:43:35 +0100

    update

--- castle-defense/army.py
+++ castle-defense/army.py
@@ -4,3 +4,4 @@ class Army:
     def __init__(self, data):
         self.strategic_map = [data[i] for i in range(1, len(data) - 1)]
         self.castle_position = data[len(data) - 1]
+        [print(i) for i in self.strategic_map]
